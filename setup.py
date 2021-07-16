0aec25612245e08cc4af973a595cb3a1c51158f0
NathanBusler <79189846+NathanBusler@users.noreply.github.com>
2021-07-16 18:32:56 +0200

    Update setup.py
    bump version to 1.0.5

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ bme68x = Extension('bme68x',
                    sources=['bme68xmodule.c', 'BME68x-Sensor-API/bme68x.c', 'internal_functions.c'])
 
 setup(name='bme68x',
-      version='1.0.4',
+      version='1.0.5',
       description='Python interface for BME68X sensor and BSEC',
       long_description=README,
       long_description_content_type='text/markdown',
